ecfe8b9117ed2d9b6a0897bacd7b46a6d5f9f55f
jonas Van Hoof <jonas.vanhoof@outlook.com>
2018-02-20 19:37:56 +0100

    Searchbar
    Added searchbar in main .search

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-/*function ToggleRecent() {
-  var item = document.getElementsByClassName('activity');
-  if (item.style.display == 'none'){
-    item.style.display = 'block';
-  }
-  else{
-    item.style.display = 'none';
-  }
-}*/
---
+++ src/js/main.ts
@@ -0,0 +1,9 @@
+/*function ToggleRecent(){
+  var item = document.getElementsByClassName('activity');
+  if (item.style.display === 'none'){
+    item.style.display = 'block';
+  }
+  else{
+    item.style.display = 'none';
+  }
+}*/
