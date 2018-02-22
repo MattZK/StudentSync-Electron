c58e9607f1507f603840a5ac7d6b794844a2ce42
jonas Van Hoof <jonas.vanhoof@outlook.com>
2018-02-22 14:02:35 +0100

    auto names/ GRID Matthias
    Added js function for auto name change.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,10 +1,15 @@
 
-function ToggleRecent(){
-  var item = document.getElementById('activity');
+function Togglepage(element:any){
+  let item:any = document.getElementById(element);
   if (item.style.display === 'none'){
     item.style.display = 'block';
   }
   else{
     item.style.display = 'none';
   }
+}
+
+function changeName(user){
+  var naam = document.getElementById('name');
+  naam.innerText = user;
 }
