3314c5340f699cc0be500d2c1d69c10377bbad68
jonas Van Hoof <jonas.vanhoof@outlook.com>
2018-02-26 12:47:30 +0100

    Delete <il>tag in Files

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,4 @@
-
+//pagina toggle
 function Togglepage(element:any){
   let item:any = document.getElementById(element);
   if (item.style.display === 'none'){
@@ -10,6 +10,25 @@ function Togglepage(element:any){
 }
 
 function changeName(user){
-  let naam = document.getElementById('name');
+  let naam:any = document.getElementById('name');
   naam.innerText = user;
 }
+
+const groups = [
+  {
+    name: 'Matthias',
+    hasNotification: false
+  },
+  {
+    name: 'Jonas',
+    hasNotification: false
+  },
+  {
+    name: 'Web Technology',
+    hasNotification: true
+  },
+  {
+    name: 'System Administration',
+    hasNotification: false
+  }
+];
